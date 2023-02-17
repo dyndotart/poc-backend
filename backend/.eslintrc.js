a62051eb53349769c0a6df6acf750153f37f81c8
benno <bennokohrs@gmail.com>
2023-02-17 06:29:48 +0100

    init

---
+++ backend/.eslintrc.js
@@ -0,0 +1,23 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    '@remotion',
+  ],
+  ignorePatterns: ['.eslintrc.js'], // https://stackoverflow.com/questions/63002127/parsing-error-parseroptions-project-has-been-set-for-typescript-eslint-parser
+  rules: {
+    'no-console': WARNING,
+    'no-var': ERROR,
+    '@typescript-eslint/no-empty-function': WARNING,
+    'prefer-const': WARNING,
+  },
+};
