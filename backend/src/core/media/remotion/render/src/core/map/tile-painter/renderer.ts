bf505eab000b621d12fbf9fc6ca0483460fa1508
benno <bennokohrs@gmail.com>
2023-02-26 07:34:06 +0100

    wip

--- backend/src/core/media/remotion/render/src/core/map/tile-painter/renderer.ts
+++
@@ -1,1 +0,0 @@
-export function initRenderer(style: any) {}
--- backend/src/core/media/remotion/render/src/stills/city-map-v1/index.tsx
+++ backend/src/core/media/remotion/render/src/stills/city-map-v1/index.tsx
@@ -2,19 +2,11 @@ import * as d3Geo from 'd3-geo';
 import { FeatureCollection, GeoJsonProperties, Geometry } from 'geojson';
 import React from 'react';
 import { Style as MapboxStyle } from '../../core/map/mapbox/types';
-
-function findHighway(key: string) {
-  return [
-    'highway_motorway_inner',
-    'highway-motorway-casing',
-    'road_major_motorway',
-    'transportation',
-    'road_motorway_casing',
-  ].includes(key);
-}
+import * as tileStencil from '../../core/map/tile-stencil';
 
 const CityMapV1: React.FC<TProps> = (props) => {
   const { style, tiles, projectionProps } = props;
+  // const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   // Define projection and geoPath, so that the map can be rendered
   // in the wished specific projection at the wished place
@@ -27,10 +19,49 @@ const CityMapV1: React.FC<TProps> = (props) => {
     .translate(projectionProps.translate)
     .precision(projectionProps.precision);
   const geoPath = d3Geo.geoPath(projection);
+  const parsedStyle = {
+    ...style,
+    layers: style.layers.map(tileStencil.getStyleFuncs),
+  };
 
-  React.useEffect(() => {
-    // TODO
-  }, []);
+  // React.useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   if (canvas == null) return;
+  //   const ctx = canvas.getContext('2d');
+  //   if (ctx == null) return;
+
+  //   const tile = tiles[0];
+  //   const layer: any = tile.layers.transportation;
+  //   const layerFuncs = style.layers.map(tileStencil.getStyleFuncs);
+  //   const parsedStyle = {
+  //     ...style,
+  //     layers: layerFuncs,
+  //   };
+  //   const parsedFilter = tileStencil.buildFeatureFilter(layer.filter);
+  //   const highwayStyle = parsedStyle.layers.find(findHighway);
+  //   const canvasGeoPath = d3Geo.geoPath(null, ctx);
+
+  //   // Select the highway features
+  //   const highwayData = {
+  //     type: 'FeatureCollection',
+  //     features: layer.features.filter(parsedFilter),
+  //   };
+
+  //   console.log({ highwayStyle, highwayData, parsedStyle, tile });
+
+  //   // ctx.fillStyle = 'white';
+  //   // ctx.fillRect(0, 0, 512, 512);
+
+  //   // Set the highway styles: translate style properties to Canvas2D settings
+  //   ctx.miterLimit = highwayStyle.layout['line-miter-limit'](tile.viewBox.z);
+  //   ctx.lineWidth = highwayStyle.paint['line-width'](tile.viewBox.z);
+  //   ctx.strokeStyle = highwayStyle.paint['line-color'](tile.viewBox.z);
+
+  //   // Draw the highways
+  //   ctx.beginPath();
+  //   canvasGeoPath(highwayData as any);
+  //   ctx.stroke();
+  // }, [canvasRef.current]);
 
   console.log({ tiles });
   if (!Array.isArray(tiles) || tiles.length <= 0) {
@@ -40,33 +71,49 @@ const CityMapV1: React.FC<TProps> = (props) => {
   return (
     <div className="flex h-full w-full items-center justify-center bg-white">
       <p className="absolute top-0 left-0 bg-white">{`
-      Projection: ${projection.center()[0]}, ${projection.center()[1]} |
-      Layers: ${tiles.length}
-      `}</p>
+       Projection: ${projection.center()[0]}, ${projection.center()[1]} |
+       Layers: ${tiles.length}
+       `}</p>
       <svg viewBox="0 0 600 600" className="bg-red-700">
-        {tiles.map((tile) => (
-          <g key={`x${tile.viewBox.x}y${tile.viewBox.y}z${tile.viewBox.z}`}>
-            <path
-              key={'boundary'}
-              fill="#7CFC00"
-              stroke="#7CFC00"
-              d={geoPath(tile.layers['building']) as string}
-            ></path>
-            <path
-              key={'water'}
-              fill="#0096FF"
-              stroke="#0096FF"
-              d={geoPath(tile.layers['water']) as string}
-            ></path>
-            <path
-              key={'transportation'}
-              fill="none"
-              stroke="#000"
-              d={geoPath(tile.layers['transportation']) as string}
-              strokeWidth="0.75"
-            ></path>
-          </g>
-        ))}
+        {tiles.map((tile) => {
+          const highwayStyle = parsedStyle.layers.find((layer) =>
+            [
+              'highway_motorway_inner',
+              'highway-motorway-casing',
+              'road_major_motorway',
+              'transportation',
+              'road_motorway_casing',
+            ].includes(layer.id)
+          );
+          const highwayStyleFilter = tileStencil.buildFeatureFilter(
+            highwayStyle.filter
+          );
+          const highwayData: FeatureCollection = {
+            type: 'FeatureCollection',
+            features: tile.layers['transportation'].features,
+            // tile.layers['transportation'].features.filter(highwayStyleFilter),
+          };
+          console.log({ tile, highwayStyle, highwayStyleFilter, highwayData });
+
+          return (
+            <g key={`x${tile.viewBox.x}y${tile.viewBox.y}z${tile.viewBox.z}`}>
+              <path
+                fill="none"
+                stroke={`rgba(${highwayStyle.paint['line-color'](
+                  tile.viewBox.z / 2
+                )})`}
+                // strokeWidth={highwayStyle.paint['line-width'](
+                //   tile.viewBox.z / 2
+                // )}
+                strokeWidth={0.75}
+                strokeMiterlimit={highwayStyle.layout['line-miter-limit'](
+                  tile.viewBox.z / 2
+                )}
+                d={geoPath(highwayData) as string}
+              />
+            </g>
+          );
+        })}
       </svg>
     </div>
   );
